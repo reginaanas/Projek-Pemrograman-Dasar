cd595fd95bb75268d33227496a7990bc3baf58bb
Regina Titian Pinasti <rtitian.pinasti@gmail.com>
2025-11-28 23:42:03 +0700

    Add files via upload

---
+++ projek/product.h
@@ -0,0 +1,31 @@
+#ifndef _PRODUCT_H
+#define _PRODUCT_H
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Product {
+public:
+    Product (std::string productName = "", int number = 0, int price = 0);
+    int _price;
+    std::string getName() const;
+    int getPrice() const;
+    int getNumber() const;
+    void newProduct();
+    static void showAllProducts();
+    static Product getProduct(int number);
+    static void editProduct(int number);
+    friend std::ostream& operator<<(std::ostream &os, const Product &p);
+
+
+private:
+    std::string _name;
+    int _number;
+
+
+public:
+    static int count;
+    static std::vector<Product> list;  
+};
+
+#endif
